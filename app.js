d8ceb37160195a0e5bcbd3dc76b1b13cc8c90562
Adela Ahmadi <adela.ahmadi1382@gmail.com>
2026-06-07 22:58:32 +0430

    feat: add centralized error handling middleware

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import menuItemRoutes from "./routes/menu.routes.js";
 import customerRoutes from "./routes/customer.routes.js";
 import orderRoutes from "./routes/order.routes.js";
 import orderItemRoutes from "./routes/orderitem.routes.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { notFound } from "./middlewares/notFound.js";
 
 const app = express();
 app.use(express.json());
@@ -13,4 +15,9 @@ app.use("/api/menu_items", menuItemRoutes);
 app.use("/api/customers", customerRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/order_items", orderItemRoutes);
+
+// Error handling middleware
+app.use(notFound);
+app.use(errorHandler);
+
 export default app;
