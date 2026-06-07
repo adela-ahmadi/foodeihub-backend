45c0047f8fe3a0b55f11edd6be0c1fb731b63813
Adela Ahmadi <adela.ahmadi1382@gmail.com>
2026-06-07 15:34:00 +0430

    refactor: centralize environment configuration

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 import express from "express";
-import dotenv from "dotenv";
 import restaurantRoutes from "./routes/restaurant.routes.js";
 import menuItemRoutes from "./routes/menu.routes.js";
 import customerRoutes from "./routes/customer.routes.js";
 import orderRoutes from "./routes/order.routes.js";
 import orderItemRoutes from "./routes/orderitem.routes.js";
-dotenv.config();
 
 const app = express();
 app.use(express.json());
